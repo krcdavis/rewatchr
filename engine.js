e69b5d1c92d813b29b0b7f548786e864c6cffd4d
krcdavis <32370620+krcdavis@users.noreply.github.com>
2023-03-30 21:17:03 -0700

    Add files via upload

--- engine.js
+++ engine.js
@@ -16,50 +16,38 @@ const tsuki = ['qO3ZTe9YENY',
 '4ESB_KjztRw',
 'l94xbILThFM'];
 
-
 window.onload = function () {
-//    player = document.getElementById("veeb");
-
-    //okay fine i'll do whatever the heck this is
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     t = d.getTime();
     t = Math.floor(t / 600000)//ten minutes
     t = t % 12
     document.getElementById("shneg").innerHTML = t
+}
 
-//    player.setAttribute('src', "https://www.youtube.com/embed/" + tsuki[t]);
-//    player.addEventListener("ended", nextID);
+  var tag = document.createElement('script');
+  tag.id = 'iframe-demo';
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('videobox', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }//ev
+    });//videobox
+  }//func
 
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('videobox', {
-          height: '390',
-          width: '640',
-          videoId: tsuki[t],
-          playerVars: {
-            'playsinline': 1
-          },
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
+  function onPlayerReady(event) {
+    document.getElementById('shneg').innerHTML = t;//test
+  }
 
-//});
-
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
+  function onPlayerStateChange(event) {
+    if event.data == YT.PlayerState.ENDED:
+        nextID()
+  }
 
 function nextID() {
  t = (t + 1) % 12
@@ -68,6 +56,6 @@ function nextID() {
 
 function loadVid(vid) {
     document.getElementById("shneg").innerHTML = t//test
-    player.setAttribute('src', "https://www.youtube.com/embed/" + tsuki[t]);
+    //player.setAttribute('src', "https://www.youtube.com/embed/" + tsuki[t]);
     console.log("help me")
 }
