65d364548aa5abb4e8d21787fa3a7ac054e403d7
krcdavis <32370620+krcdavis@users.noreply.github.com>
2023-04-26 20:11:53 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -3,19 +3,12 @@ var t = 0
 var d = new Date();
 //var player
 
-const tsuki = ['qO3ZTe9YENY',
-'HlnaIqf4Nys',
-'s7JrLG2D358',
-'RqovLOG2SnY',
-'BWsUzY50POQ',
-'Z99f_Y0ZYkQ',
-'0-aBfM2_k4Q',
-'nRGxQikzmpQ',
-'hBdOfKPAfNE',
-'s6UMSc8ZS5s',
-'4ESB_KjztRw',
-'l94xbILThFM'];
-
+const vids = {
+'interest':	['iJyweEcpsGc', 'FG4s0yxE5pY', 'qnKtglBqe78', 'Qaptvhky8IQ', '-I-umggx8Gc', 'owWtVVOSW0c','RQSBj2LKkWg'],
+'spooky':	['eZRu3Ao6zK8','SpeSpA3e56A', 'PSFY4k7KeQI', 'kKS4nm4Na-s', 'j_YznrHxphc'],
+'gaming':	['',''],
+'tsuki':	['qO3ZTe9YENY','HlnaIqf4Nys','s7JrLG2D358','RqovLOG2SnY','BWsUzY50POQ','Z99f_Y0ZYkQ','0-aBfM2_k4Q','nRGxQikzmpQ','hBdOfKPAfNE','s6UMSc8ZS5s','4ESB_KjztRw','l94xbILThFM']
+};
 
   var tag = document.createElement('script');
   tag.id = 'iframe-demo';
@@ -32,9 +25,9 @@ const tsuki = ['qO3ZTe9YENY',
     document.getElementById("shneg").innerHTML = t
 
     player = new YT.Player('videobox', {
-          height: '585',
-          width: '960',
-          videoId: tsuki[t],
+          height: '390',
+          width: '640',
+          videoId: vids.tsuki[t],
           playerVars: {
             'playsinline': 1
           },
@@ -45,7 +38,7 @@ const tsuki = ['qO3ZTe9YENY',
     });//videobox
   }//func
 
-  function onPlayerReady(event) {
+  function onPlayerReady(ebent) {
     document.getElementById('shneg').innerHTML = t;//test
   }
 
@@ -57,11 +50,11 @@ const tsuki = ['qO3ZTe9YENY',
 
 function nextID() {
  t = (t + 1) % 12
- loadVid(tsuki[t])
+ loadVid(vids.tsuki[t])
 }
 
 function loadVid(vid) {
     document.getElementById("shneg").innerHTML = t//test
     //player.setAttribute('src', "https://www.youtube.com/embed/" + tsuki[t]);
-    console.log("help me")
+    player.loadVideoById(vid);
 }
