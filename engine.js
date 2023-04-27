bed5c2d170be00f54f676547ababef5e174395b1
krcdavis <32370620+krcdavis@users.noreply.github.com>
2023-04-26 20:35:31 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,7 +1,8 @@
 //globulars
 var t = 0
 var d = new Date();
-//var player
+
+var hour = 3600000
 
 const vids = {
 'interest':	['iJyweEcpsGc', 'FG4s0yxE5pY', 'qnKtglBqe78', 'Qaptvhky8IQ', '-I-umggx8Gc', 'owWtVVOSW0c','RQSBj2LKkWg'],
@@ -20,14 +21,14 @@ const vids = {
   function onYouTubeIframeAPIReady() {
 
     t = d.getTime();
-    t = Math.floor(t / 600000)//ten minutes
-    t = t % 12
+    t = t - (t % hour)
+    
     document.getElementById("shneg").innerHTML = t
 
     player = new YT.Player('videobox', {
           height: '390',
           width: '640',
-          videoId: vids.tsuki[t],
+          videoId: vids.tsuki[0],//this is a placeholder
           playerVars: {
             'playsinline': 1
           },
@@ -36,6 +37,8 @@ const vids = {
             'onStateChange': onPlayerStateChange
         }//ev
     });//videobox
+
+    nextVid()
   }//func
 
   function onPlayerReady(ebent) {
@@ -44,10 +47,61 @@ const vids = {
 
   function onPlayerStateChange(ebent) {
     if (ebent.data == YT.PlayerState.ENDED) {
-        nextID()
+        nextVid()
     }
   }
 
+function nextVid() {
+    var tp = d.getTime();
+    var tt = tp - (tp % hour)
+    if ( tt === t - (t % hour) ) {
+        t = t + 1;
+    }
+    var vib = ""
+    var rnd = 0
+
+    switch (Math.floor(tp / hour) % 7) {
+        case 0://interest
+            rnd = seedRand(t, vids.interest.length)
+            vib = vids.interest[rnd]
+            loadVid(vib)
+            break;
+        case 1://spooky
+            rnd = seedRand(t, vids.spooky.length)
+            vib = vids.spooky[rnd]
+            loadVid(vib)
+            break;
+        case 2://gaming
+            rnd = seedRand(t, vids.gaming.length)
+            vib = vids.gaming[rnd]
+            loadVid(vib)
+            break;
+        case 3://interest
+            rnd = seedRand(t, vids.interest.length)
+            vib = vids.interest[rnd]
+            loadVid(vib)
+            break;
+        case 4://gaming
+            rnd = seedRand(t, vids.gaming.length)
+            vib = vids.gaming[rnd]
+            loadVid(vib)
+            break;
+        case 5://interest
+            rnd = seedRand(t, vids.interest.length)
+            vib = vids.interest[rnd]
+            loadVid(vib)
+            break;
+        default://spooky
+            rnd = seedRand(t, vids.spooky.length)
+            vib = vids.spooky[rnd]
+            loadVid(vib)
+            break;
+    //already i've thought of a better
+    
+    }//sw
+}
+
+/////
 function nextID() {
  t = (t + 1) % 12
  loadVid(vids.tsuki[t])
@@ -57,4 +111,21 @@ function loadVid(vid) {
     document.getElementById("shneg").innerHTML = t//test
     //player.setAttribute('src', "https://www.youtube.com/embed/" + tsuki[t]);
     player.loadVideoById(vid);
+}
+
+
+//sneeded rand
+function seedRand(seed, mod){
+ sneed = seed.toString()
+ let hash = 0;
+ for (let i = 0, len = sneed.length; i < len; i++){
+  let chr = sneed.charCodeAt(i);
+  hash =  (hash << 5) - hash + chr;
+  hash |= 0;//I don't know what this does!
+ }
+let ss = hash % mod;
+while (ss < 0) {
+ ss += mod;
+ }
+return ss;//feckin negatives
 }
