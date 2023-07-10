8363a7be47626f34c0f6f32311ed7e431585bb11
krcdavis <32370620+krcdavis@users.noreply.github.com>
2023-07-09 18:34:11 -0700

    Add files via upload

--- engine.js
+++ engine.js
@@ -3,15 +3,6 @@ var t = 0
 var hour = 3600000;
 var bonus = false;
 
-const vids = {
-'interest':	['iJyweEcpsGc', 'FG4s0yxE5pY', 'qnKtglBqe78', 'Qaptvhky8IQ', '-I-umggx8Gc', 'owWtVVOSW0c','RQSBj2LKkWg'],
-'spooky':	['eZRu3Ao6zK8','SpeSpA3e56A', 'PSFY4k7KeQI', 'kKS4nm4Na-s', 'j_YznrHxphc'],
-'gaming':	['AzTi_Gt-eSI', 'UdDf36LskNQ', 'YJEPdn_5ya4', 'OG8aTcZlyds', 'uHQ4WCU1WQc', 'Zr6xVy-9F7Q', 'mI3Xig4IfqI', 'yXbJe-rUNP8'],
-'bonus':	['PpJqrKAYFF8', 'EShUeudtaFg', '15nNY7uofNw', '29vjQwnt-Fw', 'ls3qDfTP0mw', 'PXx9TGsiwV0'],
-
-'tsuki':	['qO3ZTe9YENY', 'HlnaIqf4Nys', 's7JrLG2D358', 'RqovLOG2SnY', 'BWsUzY50POQ', 'Z99f_Y0ZYkQ', '0-aBfM2_k4Q', 'nRGxQikzmpQ', 'hBdOfKPAfNE', 's6UMSc8ZS5s', '4ESB_KjztRw', 'l94xbILThFM']
-};
-
   var tag = document.createElement('script');
   tag.id = 'iframe-demo';
   tag.src = 'https://www.youtube.com/iframe_api';
@@ -21,14 +12,9 @@ const vids = {
   var player;
   function onYouTubeIframeAPIReady() {
 
-    //t = Date.now();
-    //t = t - (t % hour)
-    
-    document.getElementById("shneg").innerHTML = t
-
     player = new YT.Player('videobox', {
-          height: 540,
-          width: 960,
+          height: '540',
+          width: '960',
           videoId: vids.tsuki[0],//this is a placeholder
           playerVars: {
             'playsinline': 1
@@ -53,11 +39,8 @@ const vids = {
 
 function nextVid() {
     var tp = Date.now();
-    //if hour of day == 4 and t % hour == 0: t += 1; tsuki[day % 12]
+    //various things
     var tt = tp - (tp % hour)
-console.log(tp)
-console.log(tt)
-console.log(t - (t % hour))
     if ( tt == t - (t % hour) ) {
         t = t + 1;
     }
@@ -66,7 +49,7 @@ console.log(t - (t % hour))
     }
     var sbeve;
     //if bonus, set sbeve to bonus and continue, else-
-    var num = Math.floor(tp / hour) % 7
+    var num = Math.floor(tp / hour) % 7//going up to 11 next
     if (bonus) { num = -1; }
 
     switch (num) {
@@ -108,16 +91,11 @@ console.log(t - (t % hour))
     }
 }
 
-/////
-function nextID() {
- t = (t + 1) % 12
- loadVid(vids.tsuki[t])
-}
 
 function loadVid(vid) {
     document.getElementById("shneg").innerHTML = t//test
-    //player.setAttribute('src', "https://www.youtube.com/embed/" + tsuki[t]);
     player.loadVideoById(vid);
+
 }
 
 
